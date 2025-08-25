f0285f8856cb2c7c4f6e881e08ecbc8d319d34db
Swati Kumari <swatikumari08072005@gmail.com>
2025-08-25 18:42:32 +0530

    navbar complete

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -17,17 +17,20 @@ function Navbar() {
             <li><a href="/">Home</a></li>
             <li><a href="/">Electric Scooters</a></li>
             <li><a href="/">Electric Bikes</a></li>
-            <li><a href="/">Support</a></li>
+            <li>
+              <a href="/" className="navLink">
+                Support <MdOutlineArrowDropDown className='dropIcon' />
+              </a>
+            </li>
           </ul>
         </div>
-        <h4>swati</h4>
         <div className={style.navInput}>
           <input type="text" placeholder="Search Bikes, Gear & Accessories" />
           <RiSearch2Line className={style.searchIcon} />
         </div>
-        <div >
-          <FaUser className='navIcons'/>
-          <RiShoppingCart2Line className='navIcons'/>
+        <div className={style.navIcons}>
+          <FaUser className='navIcons' style={{marginRight:'1rem'}}/>
+          <RiShoppingCart2Line className='navIcons' />
         </div>
       </div>
     </div>
