9fec4e3ef7b535ffb05941f29a535d1921658f92
Swati Kumari <swatikumari08072005@gmail.com>
2025-08-25 16:41:55 +0530

    navbar

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import style from "./Navbar.module.css";
+import { RiSearch2Line } from "react-icons/ri";
+import { FaUser } from "react-icons/fa6";
+import { RiShoppingCart2Line } from "react-icons/ri";
 
 function Navbar() {
   return (
-    <div className={style.header}>Navbar</div>
+    <div className={style.header}>
+      <div className={style.nav}>
+        <div>
+          <img src="https://hiboy.com.au/wp-content/uploads/2025/06/hiboy.svg" alt="dsfghj" />
+        </div>
+        <div className={style.navitem}>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Electric Scooters</a></li>
+            <li><a href="/">Electric Bikes</a></li>
+            <li><a href="/">Support</a></li>
+          </ul>
+        </div>
+        <div className={style.navInput}>
+          <input type="text" placeholder="Search Bikes, Gear & Accessories" />
+          <RiSearch2Line className={style.searchIcon} />
+        </div>
+        <div >
+          <FaUser className='navIcons'/>
+          <RiShoppingCart2Line className='navIcons'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
